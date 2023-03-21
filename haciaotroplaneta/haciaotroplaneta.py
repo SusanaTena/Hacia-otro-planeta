e300483c03fe4df1be8f631b7bb230331f2e41f6
Susana Tena <susana.tena.r@gmail.com>
2023-03-21 19:00:03 +0100

    creamos pantalla del juego

---
+++ haciaotroplaneta/haciaotroplaneta.py
@@ -0,0 +1,24 @@
+import pygame
+
+ANCHO = 1200
+ALTO = 700
+
+
+class HaciaOtroPlaneta:
+    def __init__(self):
+        pygame.init()
+        self.pantalla = pygame.display.set_mode((ANCHO, ALTO))
+
+    def comienzo(self):
+        salir = False
+        while not salir:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    salir = True
+            self.pantalla.fill((10, 20, 30))
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    juego = HaciaOtroPlaneta()
+    juego.comienzo()
